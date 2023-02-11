db0ecce9b014050421b0aaf3d70c35425d4f5527
petrikpopov <112963523+petrikpopov@users.noreply.github.com>
2023-02-11 13:10:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Class07._02._2023HM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.ForegroundColor = ConsoleColor.Red;
+            /*Employee P = new Employee();
+            P.Enter();
+            Console.WriteLine();
+            P.Print();*/
+
+            //2
+            /*Employee O = new Employee();
+            O.Name_P = "OAE";
+            O.Name_M = "Play";
+            O.Type_P = "Liner";
+            O.Print();
+            O.Enter();
+            O.Print();*/
+
+
+            //3
+            // Employee S = new Employee();
+            //S.Mass();
+
+
+            ///1
+           /*Employee P = new Employee();
+            Employee O = new Employee();
+            Console.WriteLine(P);
+            Console.WriteLine(O);
+           */
+
+            ///2
+            ///
+            /*Employee P = new Employee();
+            Employee O = new Employee();
+            Console.WriteLine();
+            Console.WriteLine(P);
+            Console.WriteLine(O);*/
+
+
+
+        }
+    }
+}
